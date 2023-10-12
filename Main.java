4882b6ec23d7df289a602366102cb83057e622e1
Angie1313 <35643364+Angie1313@users.noreply.github.com>
2023-10-12 18:22:14 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Christmas c = new Christmas();
+		c.display();
+		Halloween h = new Halloween ();
+		h.display();
+		
+		Cat cat = new Cat();
+		
+		
+		
+		
+	}
+
+}
