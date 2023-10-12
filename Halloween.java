cda4e95d212a1eceffb5b1f5c8a3e2ea7faa752b
Angie1313 <35643364+Angie1313@users.noreply.github.com>
2023-10-12 18:20:22 -0400

    Create Halloween.java

---
+++ Halloween.java
@@ -0,0 +1,9 @@
+
+public class Halloween extends Holidays {
+	String month = "October";
+	public void display() {
+		System.out.println("Halloween is in "+month+" and it makes me feel scared!");
+		super.display();
+	}
+
+}
