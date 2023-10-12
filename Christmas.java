6cfd2442e80f7947ba6e30b59f4a1306cb9f2c86
Angie1313 <35643364+Angie1313@users.noreply.github.com>
2023-10-12 18:19:51 -0400

    Create Christmas.java

---
+++ Christmas.java
@@ -0,0 +1,10 @@
+
+public class Christmas extends Holidays {
+	String month = "December";
+	public void display() {
+		System.out.println("Christmas is in "+month+" and it makes me feel so broke!");
+		super.display();
+		
+	}
+
+}
